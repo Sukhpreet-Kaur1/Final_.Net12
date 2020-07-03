c2e543fd066afdee1dd57d70545fb3e6b2235900
Sukhpreet-Kaur1 <preetsukh306@gmail.com>
2020-07-02 20:45:50 -0400

    first commit

---
+++ Final_.Net12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Final_.Net12
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            child obj = new child();
+            obj.First_Name = "preet";
+            obj.Last_Name = "kaur";
+        }
+        public class Adult
+        {
+         public string First_Name;
+            public string Last_Name;
+
+        }
+        public class child : Adult
+        { 
+        }
+    }
+}
